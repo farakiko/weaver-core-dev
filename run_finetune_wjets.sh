5bc12055d33ca0f24ba8b6b7acdf09b3f828c372
Farouk <farouk.mokhtar@gmail.com>
2026-03-16 12:23:50 +0100

    update notebook

--- run_finetune_wjets.sh
+++ run_finetune_wjets.sh
@@ -1,26 +1,66 @@
 #!/bin/bash
 
-CONFIG=v35
-PREFIX=${CONFIG}_15
+CONFIG=v36
+PREFIX=${CONFIG}_30
 # NETWORK="mlp_2p_gated_on_hid2"
 NETWORK="mlp_2p_gated_on_hid_alone"
 
 python -u weaver/train.py \
  --gpu "" \
- --batch-size 1000 --start-lr 6.75e-4 --num-epochs 10 --optimizer adam \
- --data-train 'datafiles/TaggerInput_Small/TaggerInput*/*/outfiles/train/*.root' \
+ --batch-size 1000 --start-lr 6.75e-4 --num-epochs 20 --optimizer adam \
+ --data-train 'datafiles/TaggerInput_Small_6040/TaggerInput*/*/outfiles/train/*.root' \
  --data-config weaver/data_new/finetune/FM_ak8_WJets_tagger_${CONFIG}.yaml \
  --network-config weaver/networks/fintune_test/${NETWORK}.py \
  --model-prefix experiments_finetuning/${PREFIX}/net/model \
  --log-file experiments_finetuning/${PREFIX}/logs/train.log \
  --num-workers 0 \
- --in-memory \
  --tensorboard _${PREFIX} \
- --data-test 'datafiles/TaggerInput_Small/TaggerInput*/*/outfiles/test/*.root' \
+ --data-test 'datafiles/TaggerInput_Small_6040/TaggerInput*/*/outfiles/test/*.root' \
  --load-model-weights finetune_gghww_custom \
  --export-onnx experiments_finetuning/${PREFIX}/model.onnx \
  --predict-output experiments_finetuning/${PREFIX}/predict/pred.root \
- --samples-per-epoch 800000 --samples-per-epoch-val 100000 \
+#   --in-memory \
+#  --samples-per-epoch 800000 --samples-per-epoch-val 100000 \
+
+
+
+#################### with in-memory you must use --samples-per-epoch 800000 --samples-per-epoch-val 100000
+# 100k signal: v35_12_14 (15 epochs)
+
+
+#################### without in-memory (90:10 split)
+# 100k signal: 16 (10 epochs), 17 (15 epochs)
+# 200k signal: 18 (15 epochs), 19 (10 epochs)
+
+# 100k signal: 
+# 20 (25 epochs) 20
+# 23 (35 epochs) 23
+# 26 (45 epochs) 26
+# 29 (55 epochs) 29
+
+
+
+#################### without in-memory (60:40 split)
+# 30: 20 epochs
+# 31: 30 epochs
+# 32: 40 epochs
+
+
+
+
+############### new setup with in-memory with breaks at 466 (112) in train (valid) loops
+# 100k signal: 21, 22, 23 (15 epochs), 24 (25 epochs)
+
+
+
+
+######################## Oct 15
+# v36: with invariant mass cut
+
+
+
+
+
 
 
 
